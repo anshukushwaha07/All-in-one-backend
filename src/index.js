b4910ecb8b78b0ec3b896d7a7b93b48834eaa53a
Anshu Kushwaha <anshu.kush007@gmail.com>
2025-11-02 02:13:12 +0530

    Add cookies and cors config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import dotenv from 'dotenv'
 dotenv.config(); 
 import connectDB from './db/db.js';
+import { app } from "./app.js";
+
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8080,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`)
+    })
+    app.on('error',(error)=>{
+        console.log("ERROR:",error);
+        throw error
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed !!! ",err)
+})
 
 /*
 
