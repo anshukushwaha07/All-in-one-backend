1c61958843824d0be3a8e24767440ed2b10b732f
Anshu Kushwaha <anshu.kush007@gmail.com>
2025-11-03 23:38:44 +0530

    Fix:error not found is fix

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,16 +8,3 @@ const asyncHandler = (requestHandler) => {
 
 export {asyncHandler}
 
-
-// const asyncHandler=(fn)=>async(req,res,next)=>{
-
-//     try{
-//         await fn(req,res,next)
-
-//     }catch(error){
-//         res.status(err.code ||500).json({
-//             sucess:false,
-//             message:err.message,
-//         })
-//     }
-// }
