41fb1d810366f359bcc23ce54bfeb116872dd243
Anshu Kushwaha <anshu.kush007@gmail.com>
2025-11-07 20:49:25 +0530

    Fix: update refresh token validation logic to check for absence of token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -170,7 +170,7 @@ export const generateAccessTokenAndRefreshTokens = async (userId) => {
   const refreshAccessToken = asyncHandler(async(req,res)=>{
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
 
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
       throw new ApiError(401,"Unauthorized request")
     }
 
