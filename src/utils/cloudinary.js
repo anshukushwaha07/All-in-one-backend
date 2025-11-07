cf20f99cc0e01029f08a77cf870ff850392147bb
Anshu Kushwaha <anshu.kush007@gmail.com>
2025-11-07 20:49:44 +0530

    Refactor: streamline Cloudinary upload function and remove unnecessary try-catch block

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,28 +1,73 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+// import {v2 as cloudinary} from "cloudinary"
+// import fs from "fs"
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+// cloudinary.config({ 
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+//     api_key: process.env.CLOUDINARY_API_KEY, 
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+//   });
+
+//   const uploadOnCloudinary = async (localFilePath) =>{
+//     try{
+//         if(!localFilePath) return null
+//         //Upload file in cloudinary
+
+//         console.log("From cloudinary.js API KEY:", process.env.CLOUDINARY_API_KEY);
+//         console.log("From cloudinary.js CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+//         console.log("From cloudinary.js: API SECRET", process.env.CLOUDINARY_API_SECRET);
+
+//      const response = await cloudinary.uploader.upload
+//      (localFilePath,{
+//             resource_type:"auto"
+//         })
+//         //file hab been  uploaded successfull
+//         console.log("file is uploded on cloudinary",response.url)
+//         return response
+//     }catch(error){
+//         fs.unlinkSync(localFilePath)//remove the locally save temporary files as the operation got failed
+//         return null;
+//     }
+//   }
+  
+//   export {uploadOnCloudinary}
+
+
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  // We removed the console.logs as we know they work.
+
+  if (!localFilePath) return null;
+
+          console.log("From cloudinary.js API KEY:", process.env.CLOUDINARY_API_KEY);
+        console.log("From cloudinary.js CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+        console.log("From cloudinary.js: API SECRET", process.env.CLOUDINARY_API_SECRET);
+
+
+  //
+  // NO TRY...CATCH BLOCK HERE!
+  //
+  // We let the error throw so the asyncHandler can catch it.
+  //
+
+  // Upload the file to Cloudinary
+  const response = await cloudinary.uploader.upload(localFilePath, {
+    resource_type: "auto",
   });
 
-  const uploadOnCloudinary = async (localFilePath) =>{
-    try{
-        if(!localFilePath) return null
-        //Upload file in cloudinary
+  // File has been uploaded, so now we delete the local copy
+  fs.unlinkSync(localFilePath);
 
-     const response = await cloudinary.uploader.upload
-     (localFilePath,{
-            resource_type:"auto"
-        })
-        //file hab been  uploaded successfull
-        console.log("file is uploded on cloudinary",response.url)
-        return response
-    }catch(error){
-        fs.unlinkSync(localFilePath)//remove the locally save temporary files as the operation got failed
-        return null;
-    }
-  }
-  
-  export {uploadOnCloudinary}
+  console.log("File is uploaded on Cloudinary:", response.url);
+  return response;
+};
+
+export { uploadOnCloudinary };
