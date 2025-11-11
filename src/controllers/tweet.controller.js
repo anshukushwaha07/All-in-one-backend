00f25c7259ae5795dc5059c474c712f406f8ca60
Anshu Kushwaha <anshu.kush007@gmail.com>
2025-11-11 13:17:12 +0530

    Add: implement getUserTweets function with aggregation for fetching user tweets and likes count

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -28,7 +28,58 @@ const createTweet = asyncHandler(async (req, res) => {
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-  
+    // TODO: get user tweets
+    const userId = req.user._id
+     const tweets = await User.aggregate([
+        {
+            $match: {_id: new mongoose.Types.ObjectId(userId)}
+        },
+        {
+            $lookup: {
+                from: "tweets",
+                localField: "_id",
+                foreignField: "owner",
+                as: "tweets"
+            }
+        },
+        {
+            $unwind: "$tweets"
+        },
+        {
+            $lookup:{
+                from:"likes",
+                let: { tweetId: "$tweets._id" },
+                pipeline: [
+                    { $match: { $expr: { $eq: ["$tweet", "$$tweetId"] } } },
+                    { $count: "count" }
+                ],
+                as: "likesCount"
+            }
+        },{
+            $addFields:{
+                "tweets.likesCount": { $arrayElemAt: ["$likesCount.count", 0] }
+            }
+        },{
+            $group:{
+                _id:"$_id",
+                 fullName: { $first: "$fullName" },
+                username: { $first: "$username" },
+                email: { $first: "$email" },
+                avatar: { $first: "$avatar" },
+                tweets: { $push: "$tweets" }
+            }
+        },{
+            $project:{
+                _id:1,
+                fullName:1,
+                username:1,
+                email:1,
+                avatar:1,
+                tweets:1
+            }       
+        }
+    ])
+    return res.status(200).json(new ApiResponse(200, tweets, "User Tweets fetched successfully"))    
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
