1db8e495c3a6b82708446a703be37737e7206325
Anshu Kushwaha <anshu.kush007@gmail.com>
2025-11-13 21:40:47 +0530

    Add: implement video routes with CRUD operations for videos

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import {
+  createVideo,
+  getAllVideos,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+} from "../controllers/video.controller.js";
 
 const router = Router();
+
+router.route("/videos").post(createVideo).get(getAllVideos);
+router
+  .route("/videos/:id")
+  .get(getVideoById)
+  .put(updateVideo)
+  .delete(deleteVideo);
+  
 
 export default router;
